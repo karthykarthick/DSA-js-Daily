7027f889f73587eef563844959fdd2bfe54fb99c
karthick <karthylock@gmail.com>
2024-01-19 19:21:26 +0530

    added 2 more patterns

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -1,7 +1,7 @@
 // * * *
 // * * *
 // * * *
-let str = "";
+
 function pattern1(n) {
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
@@ -32,4 +32,35 @@ function pattern3(n) {
   console.log(str);
 }
 
-pattern4(5);
+function pattern4(n) {
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      str += i;
+    }
+    str += "\n";
+  }
+  console.log(str);
+}
+
+function pattern5(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= n - i - 1; j++) {
+      str += "*";
+    }
+    str += "\n";
+  }
+  console.log(str);
+}
+
+function pattern6(n) {
+  let str = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i; j++) {
+      str += j;
+    }
+    str += "\n";
+  }
+  console.log(str);
+}
+
+pattern7(5);
